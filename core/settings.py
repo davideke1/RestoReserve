c576220cfabba44d2d11d75baf0da23daea550de
David <ekechukwudavid@gmail.com>
2023-06-18 01:35:38 +0530

    changes again

--- core/settings.py
+++ core/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import dj_database_url
-from decouple import config
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
