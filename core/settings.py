1d39f9ab481d3c61fbef33c6c4d439c5a3ea566b
David <ekechukwudavid@gmail.com>
2023-06-18 09:27:01 +0530

    changes again

--- core/settings.py
+++ core/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restoreserve',
-        'USER': 'postgres',
-        'PASSWORD': 'david',
-        'HOST': '127.0.0.1',
-        'PORT': '5433',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'restoreserve',
+#         'USER': 'postgres',
+#         'PASSWORD': 'david',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5433',
+#     }
+# }
 
 
 # Password validation
