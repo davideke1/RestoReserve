326e05fa57b7f8775d811d11ba980c2f13a2a54d
David <ekechukwudavid@gmail.com>
2023-06-18 15:43:49 +0530

    remode fam

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p9mc-lub$2k^x@=drp+rob+y$4bpsc!isa7orlc^gra^0*h(62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
